56f75f65f782955ecc6b8523cf346bcd28e5b073
Crentix <you.crentix.tube@gmail.com>
2023-05-02 00:15:52 +0200

    updated /contact

--- api/server.js
+++ api/server.js
@@ -7,32 +7,30 @@ const port = process.env.PORT || 3000;
 app.use(cors())
 app.use(express.json())
 
-app.post("/contact", sendEmail)
+app.post("/contact", async (req, res) => {
+    const { name, email, message } = req.body;
 
-async function sendEmail(req, res) {
-  const { name, email, message } = req.body;
-
-  // create reusable transporter object using the default SMTP transport
-  let transporter = nodemailer.createTransport({
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
-    secure: true,
-    auth: {
-      user: process.env.EMAIL_USER,
-      pass: process.env.EMAIL_PASSWORD,
-    },
-  });
-
-  // send mail with defined transport object
-  let info = await transporter.sendMail({
-    from: email, // sender address
-    to: process.env.EMAIL_RECIPIENT, //'buchner-bau@r-kom.net', // list of receivers
-    subject: `Nachricht von ${name} über die Website`, // Subject line
-    text: message, // plain text body
-  });
-
-  res.status(200).json({ message: 'E-Mail erfolgreich versendet' });
-}
+    // create reusable transporter object using the default SMTP transport
+    let transporter = nodemailer.createTransport({
+      host: process.env.EMAIL_HOST,
+      port: process.env.EMAIL_PORT,
+      secure: true,
+      auth: {
+        user: process.env.EMAIL_USER,
+        pass: process.env.EMAIL_PASSWORD,
+      },
+    });
+  
+    // send mail with defined transport object
+    let info = await transporter.sendMail({
+      from: email, // sender address
+      to: process.env.EMAIL_RECIPIENT, //'buchner-bau@r-kom.net', // list of receivers
+      subject: `Nachricht von ${name} über die Website`, // Subject line
+      text: message, // plain text body
+    });
+  
+    res.status(200).json({ message: 'E-Mail erfolgreich versendet' });
+})
 
 app.listen(port, () => {
     console.log(`Server listening on port ${port}.`);
