a8731f94290e461c9b97ff269556c553c1ce2c83
Crentix <you.crentix.tube@gmail.com>
2023-05-09 09:28:42 +0200

    added google maps & rate-limiting

--- api/contact.ts
+++ api/contact.ts
@@ -2,13 +2,42 @@ import type { VercelRequest, VercelResponse } from '@vercel/node';
 import { Transporter } from 'nodemailer';
 const nodemailer = require('nodemailer');
 
+// Define a Map to keep track of the requests made by each IP address
+const requestsMap: Map<string, number[]> = new Map();
+// Define a constant to limit the number of requests per IP address
+const REQUEST_LIMIT = 1;
+// Define a constant to specify the time period for the rate limiting
+const TIME_PERIOD = 60 * 60 * 1000; // 1 hour in milliseconds
+
+// Function to check if the IP address has exceeded the request limit
+function isRateLimited(ip: string): boolean {
+  const now = Date.now();
+  const requests = requestsMap.get(ip) ?? [];
+
+  // Remove any requests that are older than the time period
+  while (requests.length > 0 && requests[0] <= now - TIME_PERIOD) {
+    requests.shift();
+  }
+
+  // Add the current request to the list
+  requests.push(now);
+  // Update the requestsMap
+  requestsMap.set(ip, requests);
+
+  // Check if the number of requests has exceeded the limit
+  return requests.length > REQUEST_LIMIT;
+}
+
 export default async function handler(
   req: VercelRequest,
   res: VercelResponse,
 ) {
 
   if (req.method == "POST") {
-    console.log("incoming post request")
+    const clientIP = req.headers['x-forwarded-for'] as string;
+    if (isRateLimited(clientIP)) {
+      return res.status(429).send('Too many requests. Please try again later.');
+    }
     const { name, email, message } = req.body;
     
     // create reusable transporter object using the default SMTP transport
@@ -27,9 +56,10 @@ export default async function handler(
     // send mail with defined transport object
     transporter.sendMail({
       from: process.env.EMAIL_USER, // sender address
-      to: process.env.EMAIL_RECIPIENT, //'buchner-bau@r-kom.net', // list of receivers
-      subject: `Nachricht von ${name} über die Website`, // Subject line
-      text: `${message} von: ${email}`, // plain text body
+      to: process.env.EMAIL_RECIPIENT, // list of receivers
+      subject: `Kontaktanfrage über die Website | ${name}`, // Subject line
+      text: `Neue Kontaktanfrage von ${name} (${email}):\n${message}`, // plain text body
+      html: `<p>Neue Kontaktanfrage von ${name} (${email}):</p><br/><p>${message}</p>` // html text body
     }).then(info => {
       res.status(200).json({ message: 'E-Mail erfolgreich versendet' });
     }).catch(err => {
